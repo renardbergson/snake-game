fb84d3492243ec05e2be1f244bc48dc0fcb958be
Renard Bergson <renardrock@gmail.com>
2022-09-16 10:19:54 -0300

    renomeando variáveis de movimento e posição, iniciando keydown eventListener

--- js/main.js
+++ js/main.js
@@ -3,28 +3,47 @@ const $canvas = document.querySelector('.stage')
 const ctx = $canvas.getContext('2d')
 
 //speed game
-let speedGame = 5
+let speedGame = 4
 
 // tile settings
 let tileCount = 20
-let tileSize = $canvas.width / tileCount            
+let tileSize = $canvas.width / tileCount - 2 // the subtraction separates the tiles from each other           
 
-// snake head
-let snakeHeadX = 10                                                
-let snakeHeadY = 10                                
+// snake head and movement
+let xPosition = 10                                                
+let yPosition = 10            
+
+let xMovement = 0
+let yMovement = 0
 
 // draw game loop                                    
 function drawGame () {
     //console.log('teste')
-    setTimeout(drawGame, 1000 / speedGame)         
-
+    setTimeout(drawGame, 1000 / speedGame)       
     drawSnake()
+    changeSnakePosition()
 }
 
 // draw snake
 function drawSnake () {
     ctx.fillStyle = 'black'                         
-    ctx.fillRect(snakeHeadX * tileCount, snakeHeadY * tileCount, tileSize, tileSize)                                        
+    ctx.fillRect(xPosition * tileCount, yPosition * tileCount, tileSize, tileSize)                                        
+}
+
+//keyDown listener and function
+document.body.addEventListener('keydown', keyDown)
+function keyDown (event) {
+    // arrowUp
+    if (event.keyCode === 38) {
+        yMovement = -1 // because the Y angle has value zero in the center and decreases when goind up
+        xMovement = 0 // because we wanna do nothing with X angle when pressing arrow up key 
+    }
+}
+
+// change snake position function
+function changeSnakePosition () {
+    xPosition += xMovement
+    yPosition += yMovement
 }
 
 drawGame()
